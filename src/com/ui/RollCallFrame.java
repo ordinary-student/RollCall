61515de15bdc09187f41817c3a7cd6ab0b0e25bd
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-30 19:25:32 +0800

    Create RollCallFrame.java

---
+++ src/com/ui/RollCallFrame.java
@@ -0,0 +1,13 @@
+package com.ui;
+
+/**
+ * 点名小程序主窗口类
+ * 
+ * @author Administrator
+ *
+ */
+public class RollCallFrame extends KFrame
+{
+	private static final long serialVersionUID = 2632712392130634764L;
+
+}
