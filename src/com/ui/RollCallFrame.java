ac73118207d452ea61653a6e5292d8511e192432
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-30 19:43:01 +0800

    Update RollCallFrame.java

--- src/com/ui/RollCallFrame.java
+++ src/com/ui/RollCallFrame.java
@@ -4,6 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.SwingConstants;
 
 import com.utils.WindowUtil;
@@ -18,6 +21,7 @@ public class RollCallFrame extends KFrame
 {
 	private static final long serialVersionUID = 2632712392130634764L;
 	private JLabel label;
+	private JMenuItem editItem;
 
 	/*
 	 * 构造方法
@@ -42,9 +46,32 @@ public class RollCallFrame extends KFrame
 		// 设置关闭方式
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+		// 创建菜单栏
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setFont(new Font("宋体", Font.CENTER_BASELINE, 15));
+
+		// 创建菜单
+		JMenu settingMenu = new JMenu("设置");
+
+		// 创建菜单项
+		editItem = new JMenuItem("编辑名单");
+		editItem.addActionListener(this);
+		settingMenu.add(editItem);
+
+		menuBar.add(settingMenu);
+
+		// 设置菜单栏
+		this.setJMenuBar(menuBar);
+
 		// 标签
 		label = new JLabel("准备点名", SwingConstants.CENTER);
 		label.setFont(new Font("宋体", Font.PLAIN, 40));
 		getContentPane().add(label, BorderLayout.CENTER);
+
+		// 按钮
+
+		validate();
+		// 显示界面
+		setVisible(true);
 	}
 }
