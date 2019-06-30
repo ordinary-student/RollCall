0d53919b6219f4d84f4a9e4729e83040aae6121a
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-30 19:21:18 +0800

    Update Person.java

--- src/com/entity/Person.java
+++ src/com/entity/Person.java
@@ -1,6 +1,56 @@
 package com.entity;
 
+/**
+ * Person类
+ * 
+ * @author Administrator
+ *
+ */
 public class Person
 {
+	private int id;
+	private String name;
+
+	/*
+	 * 构造方法
+	 */
+	public Person()
+	{
+
+	}
+
+	/**
+	 * @return id
+	 */
+	public int getId()
+	{
+		return this.id;
+	}
+
+	/**
+	 * @param id
+	 *            要设置的 id
+	 */
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	/**
+	 * @return name
+	 */
+	public String getName()
+	{
+		return this.name;
+	}
+
+	/**
+	 * @param name
+	 *            要设置的 name
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 
 }
