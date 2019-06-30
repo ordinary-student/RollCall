ea1b635b277b0737904ffd14d6dbe5c16db8797f
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-30 20:29:21 +0800

    .

--- src/com/ui/SettingDialog.java
+++ src/com/ui/SettingDialog.java
@@ -15,6 +15,15 @@ public class SettingDialog extends KDialog
 	 */
 	public SettingDialog()
 	{
+		// 设置标题
+		setTitle("编辑名单");
+		// 设置大小
+		setSize(400, 300);
+		// 设置阻塞
+		setModal(true);
+		// 设置关闭方式
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
 		validate();
 		// 显示界面
 		setVisible(true);
