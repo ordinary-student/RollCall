38943ba9b5f3a62cc2316eb3a363a7ceeece863c
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-30 20:25:46 +0800

    Create EditDialog.java

---
+++ src/com/ui/EditDialog.java
@@ -0,0 +1,20 @@
+package com.ui;
+
+/**
+ * 编辑名单窗口
+ * 
+ * @author ordinary-student
+ *
+ */
+public class EditDialog extends KDialog
+{
+	private static final long serialVersionUID = -2252920404325741684L;
+
+	/**
+	 * 构造方法
+	 */
+	public EditDialog()
+	{
+	}
+
+}
