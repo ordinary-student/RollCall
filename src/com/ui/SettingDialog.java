48de2c873b90b0260c4f4bbb14820c39d4225fe9
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-30 23:28:20 +0800

    Update SettingDialog.java

--- src/com/ui/SettingDialog.java
+++ src/com/ui/SettingDialog.java
@@ -1,6 +1,9 @@
 package com.ui;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * 设置窗口类
@@ -25,8 +28,14 @@ public class SettingDialog extends KDialog
 		setLocationRelativeTo(owner);
 		// 设置阻塞
 		setModal(true);
+		// 设置布局
+		getContentPane().setLayout(new BorderLayout(10, 10));
 		// 设置关闭方式
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+		// 按钮面板
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BorderLayout());
 
 		validate();
 		// 显示界面
