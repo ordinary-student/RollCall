80b708ce5664708276170c1d37944f08eeec4eb7
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-30 19:29:14 +0800

    Update RollCallFrame.java

--- src/com/ui/RollCallFrame.java
+++ src/com/ui/RollCallFrame.java
@@ -10,4 +10,25 @@ public class RollCallFrame extends KFrame
 {
 	private static final long serialVersionUID = 2632712392130634764L;
 
+	/*
+	 * 构造方法
+	 */
+	public RollCallFrame()
+	{
+		// 初始化界面
+		initUI();
+	}
+
+	/**
+	 * 初始化界面
+	 */
+	private void initUI()
+	{
+		// 设置标题
+		setTitle("随机点名器");
+		// 设置大小
+		setBounds(200, 200, 400, 300);
+		setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 }
