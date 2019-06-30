8bb8e698b95a7f74464f1771b7e7d630872f8dc1
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-30 20:27:10 +0800

    Create SettingDialog.java

---
+++ src/com/ui/SettingDialog.java
@@ -0,0 +1,19 @@
+package com.ui;
+
+/**
+ * 设置窗口类
+ * 
+ * @author ordinary-student
+ *
+ */
+public class SettingDialog extends KDialog
+{
+	private static final long serialVersionUID = -1868260515328289597L;
+
+	/*
+	 * 构造方法
+	 */
+	public SettingDialog()
+	{
+	}
+}
