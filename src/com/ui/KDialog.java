ea57b84fa718b59782ac19af116fa2d109029ad7
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-30 20:23:45 +0800

    Create KDialog.java

---
+++ src/com/ui/KDialog.java
@@ -0,0 +1,23 @@
+package com.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JDialog;
+
+/**
+ * 自定义弹窗类
+ * 
+ * @author ordinary-student
+ *
+ */
+public class KDialog extends JDialog implements ActionListener
+{
+	private static final long serialVersionUID = -1363386749204949952L;
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+	}
+
+}
