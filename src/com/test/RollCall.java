a93f8c8e3d0437ca39b380afa524b3834dfe0c06
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-30 19:37:03 +0800

    Create RollCall.java

---
+++ src/com/test/RollCall.java
@@ -0,0 +1,32 @@
+package com.test;
+
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+import com.ui.RollCallFrame;
+
+/**
+ * 随机点名器
+ * 
+ * @author ordinary-student
+ *
+ */
+public class RollCall
+{
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			// 设置本地系统默认的窗口风格样式
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "加载本地系统窗口样式失败！");
+		} finally
+		{
+			new RollCallFrame().setVisible(true);
+		}
+	}
+}
